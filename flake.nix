26abdd0130110754ef9f7f3da0c1106da2297bd5
Brian Schroeder <bts@gmail.com>
2022-06-20 08:35:31 -0400

    Update description

--- flake.nix
+++ flake.nix
@@ -2,11 +2,9 @@
   description = "A collection of nix flake templates";
 
   outputs = { self }: {
-    templates = {
-      haskell = {
-        path = ./haskell;
-        description = "Haskell project using cabal2Nix";
-      };
+    templates.haskell = {
+      path = ./haskell;
+      description = "Haskell project using cabal2Nix";
     };
   };
 }
