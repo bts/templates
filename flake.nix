879d3869ee9e1fb0ee14a374272a17df31245ef1
Brian Schroeder <bts@gmail.com>
2022-06-20 08:36:21 -0400

    Testing

--- flake.nix
+++ flake.nix
@@ -4,7 +4,12 @@
   outputs = { self }: {
     templates.haskell = {
       path = ./haskell;
-      description = "Haskell project using cabal2Nix";
+      description = "Haskell project using cabal2Nix with support for overrides";
+    };
+
+    templates.haskell2 = {
+      path = ./haskell;
+      description = "Haskell project using cabal2Nix with support for overrides";
     };
   };
 }
